194f80488ba9f0254db623a90d420081912abbfd
Mateuskwz-NewFire <mateus.kawazoe@newfireglobal.com>
2022-07-19 19:31:50 -0300

    revert: rollback the dashboard page to create 2 distinct commits

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
