7eae43478078cadbf3d2ca131561eea994a1f12d
Mateuskwz-NewFire <mateus.kawazoe@newfireglobal.com>
2022-07-19 10:47:43 -0300

    chore: refactoring the project structure and adding some libs/configs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'login-background': "url('../src/resource/images/background.jpg')",
+      },
+      colors: {
+        'newfire-blue': '#3F65BA'
+      }
+    },
+  },
+  plugins: [],
+}
