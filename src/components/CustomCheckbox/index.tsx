93f74fdc791f5852dfa34f55e2ba708ce8b1557f
Mateuskwz-NewFire <mateus.kawazoe@newfireglobal.com>
2022-07-19 12:40:53 -0300

    feat: custom checkbox component

---
+++ src/components/CustomCheckbox/index.tsx
@@ -0,0 +1,25 @@
+import { MutableRefObject } from "react";
+
+export default function CustomCheckbox(props: {
+  label: string;
+  refs: MutableRefObject<HTMLInputElement[]>;
+}) {
+  return (
+    <div className="custom-input-container flex w-full pl-1">
+      <input
+        className="w-4 accent-amber-600"
+        type="checkbox"
+        id={`my-input-${props.label}`}
+        ref={(e) => {
+          if (e) props.refs.current.push(e);
+        }}
+      />
+      <label
+        className="text-lg pt-0.5 ml-4 pb-0.5"
+        htmlFor={`my-input-${props.label}`}
+      >
+        {props.label}
+      </label>
+    </div>
+  );
+}
