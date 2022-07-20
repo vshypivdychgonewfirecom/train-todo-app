1282682f60e48138f1cd68f2c33680972355d961
Mateuskwz-NewFire <mateus.kawazoe@newfireglobal.com>
2022-07-20 01:20:43 -0300

    feat: creating simple dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function DashBoard() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (
+      !localStorage.getItem("newfire-train-todo-app-token") &&
+      !sessionStorage.getItem("newfire-train-todo-app-token")
+    ) {
+      navigate("../login", { replace: true });
+    }
+  }, []);
+
+  return (
+    <div className="home-container flex flex-col">
+      Hello World
+      <button
+        className="bg-orange-600 text-white w-32 rounded-md mt-10 cursor-pointer hover:bg-orange-400 active:bg-orange-700"
+        onClick={() => {
+          if (localStorage.getItem("newfire-train-todo-app-token")) {
+            localStorage.setItem("newfire-train-todo-app-token", "");
+          }
+
+          sessionStorage.setItem("newfire-train-todo-app-token", "");
+          navigate("../login", { replace: true });
+        }}
+      >
+        Log out
+      </button>
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
